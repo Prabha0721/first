6acea187bd9805dffc88ee19029c09f6737e1ff2
Prabha0721 <147272590+Prabha0721@users.noreply.github.com>
2024-10-26 08:39:47 +0530

    Update sitemap.py

--- sitemap.py
+++ sitemap.py
@@ -3,3 +3,9 @@ st.header("To Extract Urls")
 
 user_input = st.text_input("Enter sitemap url:")
 st.write(user_input)
+
+import advertools as adv
+  import pandas as pd
+
+  sitemap = adv.sitemap_to_df('https://www.zoho.com/blog/payments/sitemap.xml')
+  sitemap.to_csv("output.csv")
